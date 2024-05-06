eba44e11aa7fb6f4ba55f42098e8a48d0a7a4ebd
wereDevelopers <realweredevelopers@gmail.com>
2024-05-06 13:53:04 +0200

    new intent/bundle getSerializable utils methods

--- library/binding-ui/src/main/java/com/weredev/binding_ui/Ext.kt
+++ library/binding-ui/src/main/java/com/weredev/binding_ui/Ext.kt
@@ -1,10 +1,14 @@
 package com.weredev.binding_ui
 
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
+import java.io.Serializable
 
 /**
  * This inline function simplifies the process of creating ViewBinding within an Android Activity.
@@ -28,4 +32,30 @@ inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
  */
 fun ViewGroup.inflate(layoutRes: Int): View {
     return LayoutInflater.from(context).inflate(layoutRes, this, false)
+}
+
+/**
+ * Retrieves a serializable object of type T from the extras of an Intent using the provided key.
+ *
+ * @param key The key used to retrieve the serializable object from the Intent extras.
+ * @return The deserialized object of type T, or null if the key is not found or the deserialization fails.
+ * @param T The expected type of the deserialized object.
+ */
+public inline fun <reified T> Intent?.getSerializable(key: String): T? {
+    return this?.extras?.getSerializableFromBundle(key)
+}
+
+/**
+ * Retrieves a serializable object of type T from a Bundle using the provided key.
+ *
+ * @param key The key used to retrieve the serializable object from the Bundle.
+ * @return The deserialized object of type T, or null if the key is not found or the deserialization fails.
+ * @param T The expected type of the deserialized object.
+ */
+public inline fun <reified T> Bundle?.getSerializableFromBundle(key: String): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        this?.getSerializable(key, Serializable::class.java) as T?
+    } else {
+        this?.getSerializable(key) as T?
+    }
 }
